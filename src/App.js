708a88b9dc0d583fa687c6e3b48d4a8a8fa204d8
Narender-Chahal <nc7206377341@gmail.com>
2023-05-02 11:07:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
+import AboutSection from "./components/AboutSection";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <AboutSection />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+function AboutSection() {
+  return (
+    <section>
+      <Container>
+        <div className="ff_highspeed fs_xsm">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita
+          debitis minus nobis dolorum, commodi odit ad necessitatibus maxime
+          praesentium recusandae voluptas earum optio perspiciatis tenetur
+          aperiam temporibus consequuntur. Tempora, fuga.
+        </div>
+      </Container>
+    </section>
+  );
+}
+
+export default AboutSection;
